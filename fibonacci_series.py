148b4d31ab3545a16f938f2cf2822e156b33a910
pranjalmishra990 <51012460+pranjalmishra990@users.noreply.github.com>
2019-05-25 18:34:23 +0530

    Add files via upload

---
+++ fibonacci_series.py
@@ -0,0 +1,23 @@
+p = 0
+m = 1
+r = 0
+z = int(raw_input("how many fibonacci numbers you want? "))
+while(r < z):
+    print p
+    p=p+m
+    (p,m)=(m,p)
+    r = r + 1
+
+
+
+
+
+
+
+
+
+
+    
+    
+     
+
